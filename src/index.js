cbbab261d30bdc06c02c1d70a103ac795fa5b859
Ray Bergholm <raybergholm@gmail.com>
2018-04-10 22:09:21 +0300

    add foundation distributable

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import "./index.css";
 // Bootstrap
 // import "bootstrap/dist/css/bootstrap.min.css";
 
+// Foundation
+import "foundation-sites/dist/css/foundation.min.css";
+
+import foundation from "foundation-sites/dist/js/foundation.min.js";
+
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
