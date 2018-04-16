f29a9896cd3f12348856981f6b08f06c632c6da0
Ray Bergholm <raybergholm@gmail.com>
2018-04-16 19:42:51 +0300

    add foundation library generic wrapper

---
+++ src/components/foundation/Foundation.js
@@ -0,0 +1,13 @@
+import Card from "./Card";
+import Menu from "./Menu";
+import MenuItem from "./MenuItem";
+import TopBar from "./TopBar";
+import XyGrid from "./XyGrid";
+
+export default {
+  Card,
+  Menu,
+  MenuItem,
+  TopBar,
+  XyGrid
+};
