a02ec7f2fcaa02b4a06d849918930d3efca539e6
Ray Bergholm <raybergholm@gmail.com>
2018-04-16 15:36:04 +0300

    consolidate blog feed view

--- src/pages/blog/Feed.js
+++ src/pages/blog/Feed.js
@@ -1,8 +1,20 @@
 import React from "react";
 
+import FlexColumnContainer from "../../components/layout/FlexColumnContainer";
+
+const fetchMockContent = () => {
+
+    return {
+        mainContent: {},
+        rightContent: {}
+    };
+};
+
 const Page = () => (
     <div>
         <p>I am the blog feed page</p>
+
+        <FlexColumnContainer {...fetchMockContent()} />
     </div>
 );
 
