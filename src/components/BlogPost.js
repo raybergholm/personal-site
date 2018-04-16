08c0472f89fc0a1525eb7b3d0b7a3c13e999bed0
Ray Bergholm <raybergholm@gmail.com>
2018-04-16 16:15:18 +0300

    add blog post linking functionality

--- src/components/BlogPost.js
+++ src/components/BlogPost.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 
 import Showdown from "showdown";
 
+const ARTICLE_ROOT_URL = "/blog/";
+
 const converter = new Showdown.Converter({
   headerLevelStart: 3,
   requireSpaceBeforeHeadingText: true,
@@ -12,9 +14,9 @@ const converter = new Showdown.Converter({
 
 const parseText = (text) => converter.makeHtml(text);
 
-const BlogPost = ({ _id, title, timestamp, author, body }) => (
+const BlogPost = ({ _id, link, title, timestamp, author, body }) => (
   <div id={`blog-post-${_id}`}>
-    <h2>{title}</h2>
+    <h2><a href={`${ARTICLE_ROOT_URL}${link}`}>{title}</a></h2>
     <p>{timestamp} by {author}</p>
     <hr />
     <div dangerouslySetInnerHTML={{__html: parseText(body)}} />
@@ -23,10 +25,11 @@ const BlogPost = ({ _id, title, timestamp, author, body }) => (
 
 BlogPost.propTypes = {
   _id: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  timestamp: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
-  body: PropTypes.object.isRequired
+  body: PropTypes.object.isRequired,
+  link: PropTypes.string.isRequired,
+  timestamp: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
 };
 
 export default BlogPost;
