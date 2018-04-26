3cfe18a86ea0a6517bb25ebbc840ec8ed0ef65a6
Ray Bergholm <raybergholm@gmail.com>
2018-04-26 11:01:53 +0300

    bugfix invalid BlogPost input, remove pagination

--- src/pages/blog/Feed.js
+++ src/pages/blog/Feed.js
@@ -1,31 +1,19 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Pagination } from "../../components/foundation/Foundation";
-
 import TwoColumnRightBody from "../../components/layout/TwoColumnRightBody";
 import BlogPost from "../../components/BlogPost";
 import BlogSidebar from "../../components/layout/BlogSidebar";
 
 import mockPosts from "../../config/mockPosts.json";
 
-const getPageNumber = (params) => params.pageNumber;
-
-const fetchMainContent = (pageNumber) => {
-  const posts = mockPosts.map((props) => (<BlogPost key={props._id} {...props} />));
-
-  const paginationSettings = {
-    currentIndex: 1,
-    pageCount: 12
-  };
-  const pagination = (<Pagination key="pagination" {...paginationSettings} />);
-
-  return [posts, pagination];
+const fetchMainContent = () => {
+  return mockPosts.map((post) => (<BlogPost key={post._id} _id={post._id} {...post.content} />));
 };
 
-const Page = ({ match }) => (
+const Page = () => (
   <TwoColumnRightBody id="main-content-section" side={<BlogSidebar topAnchor="app-header:bottom" bottomAnchor="app-footer:top" />}>
-    {fetchMainContent(getPageNumber(match))}
+    {fetchMainContent()}
   </TwoColumnRightBody>
 );
 
